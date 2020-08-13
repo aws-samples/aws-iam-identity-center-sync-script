583b1da33d05035a39d4f2e2813ae13bd0952bcc
Aidan Keane <67668465+ajkeane-aws@users.noreply.github.com>
2020-08-13 12:10:08 -0400

    Initial Pull Request (#1)
    * Commit 1
    
    * Commit 2
    
    * Commit 3
    
    * Commit 4
    
    * Commit 5
    
    * Commit 6
    
    * Commit 8
    
    * Commit 5
    
    * updated readme
    
    * Updated Readme for spaces

--- README.md
+++ README.md
@@ -1,11 +1,90 @@
-## My Project
+## AWS-SSO-SYNC
 
-TODO: Fill this README out!
+## Overview
+On Demand SCIM provisioning of Azure AD to AWS SSO with PowerShell
 
-Be sure to:
+## Getting Started
 
-* Change the title in this README
-* Edit your repository description on GitHub
+### Prerequisites
+
+Configure **AWS Single Sign-On** per the steps outlined in this [article](https://aws.amazon.com/blogs/aws/the-next-evolution-in-aws-single-sign-on/)
+
+### App Registration
+
+#### Create
+
+- Navigate to Azure Active Directory
+- Open the _App Registrations_ blade
+- Choose _New Registration_
+  - Name: [_Example: AWS_]
+  - All other options remain default
+- Choose _Register_
+- Open the _Certificates & secrets_ blade
+- Choose _New client secret_
+- Choose _Add_
+- Copy the Value shown within Client Secrets
+
+#### Configure
+
+- Navigate to Azure Active Directory
+- Open the _App Registrations_ blade
+- Choose the app created in the previous task
+- Open the _API Permissions_ blade
+- Choose _Add a permission_
+- Choose _Microsoft Graph_
+- Choose _Delegated permissions_
+- Scroll down to _Directory_ and expand
+- Choose _Directory.ReadWrite.All_
+- Choose _Add permissions_
+- Choose _Grant admin consent for [Tenant Name]_
+
+
+### Service Account
+
+- Navigate to Azure Active Directory
+- Choose _Users_ blade
+- Choose _New user_
+  - User name: [_Example: svc.aws_]
+  - Name: [_Example: Service Account (AWS)_]
+  - Password: _Let me create the password_
+    - Provide secure value
+- Choose _Create_
+- Navigate to new user
+- Choose _Assigned roles_
+- Choose _Add assignments_
+- Scroll down and Choose _Global administrator_
+
+
+
+### Enterprise Application
+
+- Navigate to Azure Active Directory
+- Choose _Enterprise applications_ blade
+- Choose the app created in the previous task
+- Choose _Users and groups_ blade
+- Choose _Add user_
+- Choose _Users_
+- Choose service account created in previous task
+- Choose _Assign_
+
+### Inputs
+
+Gather the following properties for input into the PowerShell script
+
+* Navigate to Azure Active Directory
+* Choose _App Registrations_ blade
+* Choose the app created in the previous task
+* Copy the following values:
+  - Tenant Id
+  - Application Name
+  - Application Id
+  - Client Secret
+  - Username
+  - Password
+
+> Note: When copying and pasting in Windows, choose the PowerShell icon, then Edit > Paste.
+
+
 
 ## Security
 
---
+++ Sync.ps1
@@ -0,0 +1,265 @@
+#Requires -Version 7.0
+#region Get
+function Get-GraphSynchronizationJobId() {
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [securestring]$AccessToken,
+ 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$ServicePrincipalId
+    )
+ 
+    begin {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " begin")
+        $params = @{
+            Method  = "GET"
+            Uri     = ("https://graph.microsoft.com/beta/servicePrincipals/" + $servicePrincipalId + "/synchronization/jobs")
+            Headers = @{
+                "Authorization" = ("Bearer " + (ConvertFrom-SecureString -SecureString $accessToken -AsPlainText))
+                "Accept"        = "application/json"
+            }
+            Body    = @{ }
+        }
+    }
+ 
+    process {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " process")
+        try {
+            $response = Invoke-RestMethod @params
+            $response = $response.value[0].id
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -ErrorAction Stop
+        }
+    }
+ 
+    end {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " end")
+        return $response
+    }
+ 
+}
+function Get-GraphServicePrincipal() {
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [securestring]$AccessToken,
+ 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$DisplayName
+    )
+ 
+    begin {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " begin")
+        $params = @{
+            Method  = "GET"
+            Headers = @{
+                "Authorization" = ("Bearer " + (ConvertFrom-SecureString -SecureString $accessToken -AsPlainText))
+                "Accept"        = "application/json"
+            }
+        }
+    }
+ 
+    process {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " process")
+        try {
+            $response = Invoke-RestMethod @params -Uri "https://graph.microsoft.com/beta/servicePrincipals"
+            $response.value | ForEach-Object {
+                if ($_.displayName -like $displayName) {
+                    $objectId = $_.id
+                }
+            }
+            if ($response.'@odata.nextLink') {
+                Write-Verbose -Message "NextLink Section"
+                $nextLink = $response.'@odata.nextLink'
+                while ($nextLink -ne $null) {
+                    $output = Invoke-RestMethod @params -Uri $nextLink
+                    $output.value | ForEach-Object {
+                        if ($_.displayName -like $displayName) {
+                            $objectId = $_.id
+                        }
+                    }
+                    $nextLink = $output.'@odata.nextLink'
+ 
+                }
+            }
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -ErrorAction Stop
+        }
+    }
+ 
+    end {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " end")
+        return $objectId
+    }
+ 
+}
+#endregion
+
+#region New
+function New-GraphAccessToken() {
+
+    [CmdletBinding()]
+    [OutputType([securestring])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$TenantId,
+ 
+        [Parameter(Mandatory = $true)]
+        [string]$ApplicationId,
+ 
+        [Parameter(Mandatory = $true)]
+        [securestring]$ClientSecret,
+ 
+        [Parameter(Mandatory = $true)]
+        [string]$Username,
+ 
+        [Parameter(Mandatory = $true)]
+        [securestring]$Password
+    )
+ 
+    begin {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " begin")
+        $params = @{
+            Method  = "POST"
+            Uri     = ("https://login.microsoftonline.com/" + $tenantId + "/oauth2/token")
+            Headers = @{
+                "Content-Type" = "application/x-www-form-urlencoded"
+                "Accept"       = "application/json"
+            }
+            Body    = @{
+                "resource"      = "https://graph.microsoft.com"
+                "grant_type"    = "password"
+                "client_id"     = "$applicationId"
+                "client_secret" = "$(ConvertFrom-SecureString -SecureString $clientSecret -AsPlainText)"
+                "username"      = "$username"
+                "password"      = "$(ConvertFrom-SecureString -SecureString $password -AsPlainText)"
+                "scope"         = "openid"
+            }
+        }
+    }
+ 
+    process {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " process")
+        try {
+            $response = Invoke-RestMethod @params
+            $accessToken = ConvertTo-SecureString -String "$($response.access_token)" -AsPlainText -Force
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -ErrorAction Stop
+        }
+    }
+ 
+    end {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " end")
+        return $accessToken
+    }
+ 
+}
+#endregion
+
+#region Start
+function Start-GraphSynchronizationJob() {
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [securestring]$AccessToken,
+ 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$ServicePrincipalId,
+ 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [string]$JobId
+    )
+ 
+    begin {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " begin")
+        $params = @{
+            Method  = "POST"
+            Uri     = ("https://graph.microsoft.com/beta/servicePrincipals/" + $servicePrincipalId + "/synchronization/jobs/" + $JobId + "/start")
+            Headers = @{
+                "Authorization" = ("Bearer " + (ConvertFrom-SecureString -SecureString $accessToken -AsPlainText))
+            }
+        }
+    }
+ 
+    process {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " process")
+        try {
+            $response = Invoke-RestMethod @params
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -ErrorAction Stop
+        }
+    }
+ 
+    end {
+        Write-Verbose -Message ("Initiating function " + $MyInvocation.MyCommand + " end")
+        return $response
+    }
+ 
+}
+#endregion
+
+#region Initialization
+function Initialization {
+
+    [CmdletBinding()]
+    param ()
+
+    begin {
+        Clear-Host
+        Write-Host "AWS Single Sign-On Integration - Sync`n" -ForegroundColor Yellow
+
+        $tenantId = Read-Host -Prompt "Tenant Id"
+        if ([string]::IsNullOrEmpty($tenantId)) { Write-Error -Message "Tenant Id cannot be blank." -ErrorAction Stop }
+
+        $displayName = Read-Host -Prompt "Display Name"
+        if ([string]::IsNullOrEmpty($displayName)) { Write-Error -Message "Display Nane cannot be blank." -ErrorAction Stop }
+
+        $applicationId = Read-Host -Prompt "App Id"
+        if ([string]::IsNullOrEmpty($applicationId)) { Write-Error -Message "Application Id cannot be blank."  -ErrorAction Stop }
+
+        $clientSecret = Read-Host -Prompt "Client Secret" -AsSecureString
+        if ([string]::IsNullOrEmpty($clientSecret)) { Write-Error -Message "Client Secret cannot be blank."  -ErrorAction Stop }
+
+        $username = Read-Host -Prompt "Username"
+        if ([string]::IsNullOrEmpty($username)) { Write-Error -Message "Username cannot be blank."  -ErrorAction Stop }
+
+        $password = Read-Host -Prompt "Password" -AsSecureString
+        if ([string]::IsNullOrEmpty($password)) { Write-Error -Message "Password cannot be blank."  -ErrorAction Stop }
+        
+        Clear-Host
+        Write-Host "AWS Single Sign-On Integration - Sync Starting" -ForegroundColor Yellow
+    }
+
+    process {
+        $accessToken = New-GraphAccessToken -TenantId $tenantId -ApplicationId $applicationId -ClientSecret $clientSecret -Username $username -Password $password
+        $servicePrincipalId = Get-GraphServicePrincipal -AccessToken $accessToken -DisplayName $displayName
+        $jobId = Get-GraphSynchronizationJobId -AccessToken $accessToken -ServicePrincipalId $servicePrincipalId
+        Start-GraphSynchronizationJob -AccessToken $accessToken -ServicePrincipalId $servicePrincipalId -JobId $jobId
+    }
+
+    end {
+        Write-Host "AWS Single Sign-On Integration - Sync Completed" -ForegroundColor Green
+    }
+
+}
+#endregion
+
+Initialization
